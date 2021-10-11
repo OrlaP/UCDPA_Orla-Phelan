f6d70b913c4aa8e1b762ccb5ed275161a4dd23b9
OrlaP <orlaphelan@ymail.com>
2021-10-11 20:51:10 +0100

    week 7 merging data frames

--- World_wines.py
+++ World_wines.py
@@ -1,8 +1,11 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('winemag-data-130k-v2.csv')
 
 print(df.shape)
+print(df.head())
+print(df.tail())
 
 missing_values_count = df.isnull().sum()
 print(missing_values_count)
@@ -14,4 +17,26 @@ print(df.loc[:,"price"])
 # with median of that column
 df['price'] = df['price'].fillna(df['price'].median())
 print(df['price'])
+# selecting designation column
+print(df.loc[:,'designation'])
+# removed column 'designation'
+print(df.drop(['designation'], axis=1))
+# create wine dictionary
+wine = {'country', 'description', 'points', 'price', 'province', 'title', 'variety', 'title'}
+
+df2 = pd.DataFrame(wine)
+
+print(df2)
+
+#api request
+import requests
+request = requests.get('https://api.globalwinescore.com/')
+print(request.status_code)
+
+
+
+
+
+
+
 
