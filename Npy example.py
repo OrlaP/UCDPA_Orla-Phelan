86713c2b36e85abcc69b0dfec327031c54b57dbb
OrlaP <orlaphelan@ymail.com>
2021-11-15 21:53:56 +0000

    project files

---
+++ Npy example.py
@@ -0,0 +1,19 @@
+import numpy as np
+from numpy import random
+import pandas as pd
+
+df = pd.read_csv('C:/Users/User/PycharmProjects/pythonProject/data files/heart.csv')
+
+heart = np.array(df[1:], dtype=float)
+
+print(heart)
+
+x = random.randint(1000)
+
+print(x)
+
+x1 = random.randint(1000, size=(4, 6))
+
+print(x1)
+
+print(np.__version__)
