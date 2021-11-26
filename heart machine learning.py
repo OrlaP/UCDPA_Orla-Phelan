643ab56e03a2ffa156046b15e7f974d1884ed8b9
OrlaP <orlaphelan@ymail.com>
2021-11-26 16:32:58 +0000

    project files

--- heart machine learning.py
+++ heart machine learning.py
@@ -13,24 +13,21 @@ heart_features = ['sex', 'chol', 'exang']
 
 X = heart_stat[heart_features]
 
-print (X.describe())
+print(X.describe())
 
-heart_model = DecisionTreeRegressor(max_depth = 5, random_state=1234)
+heart_model = DecisionTreeRegressor(max_depth=3, random_state=1234)
 
-heart_model.fit(X,y)
+heart_model.fit(X, y)
 
 print(X.head())
 print("The predictions are")
 print(heart_model.predict(X.head()))
 
 text_representation = tree.export_text(heart_model)
-print ("The report")
+print("The report")
 print(text_representation)
 
-fig = plt.figure(figsize=(12,9))
+fig = plt.figure(figsize=(12, 9))
 _ = tree.plot_tree(heart_model, feature_names=None, filled=True)
 
 plt.show()
-
-
-
