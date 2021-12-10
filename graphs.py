27c88e4493a95ab33cd128c7f0636e358145f0be
OrlaP <orlaphelan@ymail.com>
2021-12-10 15:56:11 +0000

    Project files

--- graphs.py
+++ graphs.py
@@ -1,9 +1,7 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from scipy import stats
 
 heart = pd.read_csv("C:/Users/User/PycharmProjects/UCD-Project-/data files/heart.csv")
-sns.catplot(x= 'age', y='chol', hue='sex',  data=heart)
+sns.catplot(x='age', y='chol', hue='sex',  data=heart)
 plt.show()
